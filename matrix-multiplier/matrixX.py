a3483fd42086fa17412b0e6b36c79d3aa37a6932
Renato Bittencourt <renato.pereira@atos.net>
2020-03-16 12:02:52 -0300

    add identity matrix and mutiples folders for each result

--- matrix-multiplier/matrixX.py
+++ matrix-multiplier/matrixX.py
@@ -1,6 +1,39 @@
 '''
 Matrix multiplier - by Renato Bittencourt
+GRR20182103
 '''
+
+import datetime
+import os
+
+
+currentDT = datetime.datetime.now()
+curDT = str(currentDT).replace("-", "_").replace(" ", "_").replace(":", "-").split(".")[0]
+
+folder = "result_" + curDT
+os.mkdir("./matrix/" + folder)
+
+
+def createIdentityMatrix(n):
+    identity = [0] * int(n)
+    for u in range(0, int(n)):
+        identity[u] = [0] * int(n)
+        identity[u][u] = 1
+    return identity
+
+
+def saveMatrix(matrix, name):
+    result = open("./matrix/" + folder + "/" + name + ".txt", "w+")
+    for j in range(0, len(matrix)):
+        for k in range(0, len(matrix[0])):
+            if k < len(matrix[0]) - 1:
+                result.write("%d\t" % int(matrix[j][k]))
+            else:
+                result.write("%d" % int(matrix[j][k]))
+        if j < len(matrix) - 1:
+            result.write("\n")
+    print(matrix)
+
 
 # open files to get matrix
 matrix1File = open("./matrix/matrix1.txt", "r")
@@ -14,6 +47,7 @@ matrix2String = matrix2File.read()
 matrix1Rows = matrix1String.split("\n")
 matrix2Rows = matrix2String.split("\n")
 
+# print(matrix1String, matrix2String)
 # save rows number of matrix
 r1 = len(matrix1Rows)
 r2 = len(matrix2Rows)
@@ -24,17 +58,29 @@ matrix2 = [0]*r2
 
 # split each row to get elements
 for r in range(r1):
-    matrix1[r] = matrix1Rows[r].split(" ")
+    matrix1[r] = matrix1Rows[r].split("\t")
 for r in range(r2):
-    matrix2[r] = matrix2Rows[r].split(" ")
+    matrix2[r] = matrix2Rows[r].split("\t")
 
 # save column number of matrix
 c1 = len(matrix1[0])
-c2 = len(matrix1[2])
+c2 = len(matrix2[0])
+
+# verify its not about identity matrix definition
+if r1 == 1 and c1 == 1:
+    r1 = int(matrix1[0][0])
+    c1 = int(matrix1[0][0])
+    matrix1 = createIdentityMatrix(matrix1[0][0])
+saveMatrix(matrix1, "matrix1")
+if r2 == 1 and c2 == 1:
+    r2 = int(matrix2[0][0])
+    c2 = int(matrix2[0][0])
+    matrix2 = createIdentityMatrix(matrix2[0][0])
+saveMatrix(matrix2, "matrix2")
 
 # validate matrix indexes
-
 if c1 != r2:
+    print(c1, " != ", r2)
     # matrix don't matches
     print("ERRO: O número de colunas da matriz 1 deve ser igual ao número de linhas da matriz 2.")
 else:
@@ -51,12 +97,5 @@ else:
                 matrixResult[r3][c3] += int(matrix1[r3][i]) * int(matrix2[i][c3])
 
     # save new matrix on file
-    result = open("./matrix/RESULT.txt", "w+")
-    for j in range(0, len(matrixResult)):
-        for i in range(0, len(matrixResult[0])):
-            result.write("%d\t" % (matrixResult[j][i]))
-        result.write("\n")
-    print(matrixResult)
-
-
+    saveMatrix(matrixResult, "RESULT")
 
